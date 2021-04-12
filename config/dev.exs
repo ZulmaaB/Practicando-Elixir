91ba639933f9425ec3c21bc2206d7129c26d3590
Zulma <zulma_rozzii@hotmail.com>
2021-04-12 12:15:01 -0500

    feat: migration && docker setup

--- config/dev.exs
+++ config/dev.exs
@@ -2,9 +2,9 @@ use Mix.Config
 
 # Configure your database
 config :hellow, Hellow.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "hellow_dev",
+  username: "homestead",
+  password: "secret",
+  database: "homestead",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
